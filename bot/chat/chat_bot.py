7c0e0702ea6164a8fc434d2f176362ac0f93116b
chacha86 <taejincha0619@gmail.com>
2023-07-12 20:16:20 +0900

    resources add

--- bot/chat/chat_bot.py
+++ bot/chat/chat_bot.py
@@ -3,18 +3,29 @@ from bot.chat import constants as co
 import re
 import numpy as np
 from joblib import load
+from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from konlpy.tag import Okt
 from flask import current_app
+import warnings
+
+warnings.filterwarnings('ignore')
 
 class ChatBot():
 
     def __init__(self):
+        self.okt = Okt()
+        self.max_length = 30
+        self.padding_size = self.max_length
+        self.tokenizer = Tokenizer(filters='', lower=False, oov_token='<OOV>')
+        self.num_of_epochs = 20
+        self.batch_size = 16
+
+    def load_test(self):
         base_dir = current_app.root_path
         name = '/resource/tokenizer'
         self.tokenizer = load(base_dir + name)
 
-        self.okt = Okt()
         self.DATA_LENGTH = co.DATA_LENGTH
         START_TOKEN = self.tokenizer.word_index['<START>']
         END_TOKEN = self.tokenizer.word_index['<END>']
@@ -22,12 +33,23 @@ class ChatBot():
         self.bot = Seq2seq(co.UNITS, co.VOCAB_SIZE, co.EMBEDDING_DIM, co.TIME_STEPS, START_TOKEN, END_TOKEN)
         self.bot.load_weights(base_dir + '/resource/test')
 
-
+    def get_vocab_size(self):
+        return len(self.tokenizer.index_word) + 1
     def get_tokenizer(self):
         return self.tokenizer
 
     def get_bot(self):
         return self.bot
+
+    def fit_texts(self, sentences):
+        self.tokenizer.fit_on_texts(sentences)
+        self.start_token_idx = self.tokenizer.word_index['<START>']
+        self.end_token_idx = self.tokenizer.word_index['<END>']
+
+    def get_sequences(self, sentences):
+        sequences = self.tokenizer.texts_to_sequences(sentences)
+        return sequences
+
     ## 모델이 예측한 인코딩된 값을 다시 문자로 디코딩 해주는 함수
     def convert_index_to_text(self, indexes, end_token):
         sentence = ''
@@ -82,3 +104,37 @@ class ChatBot():
     def clean_sentence(self, sentence):
         sentence = re.sub(r'[^0-9ㄱ-ㅎㅏ-ㅣ가-힣 ]', r'', sentence)
         return sentence
+
+    def preprocess(self, texts, pairs):
+        questions = []
+        answer_in = []
+        answer_out = []
+
+        ## 질문에 대한 전처리
+        for text in texts:
+            question = self.clean_and_morph(text, is_question=True)
+            questions.append(question)
+
+        ## 답변에 대한 전처리
+        for pair in pairs:
+            in_, out_ = self.clean_and_morph(pair, is_question=False)
+            answer_in.append(in_)
+            answer_out.append(out_)
+
+        return questions, answer_in, answer_out
+
+    def pad_sequences(self, seq_list, max_len, trun, padd):
+        return list(map(
+            lambda x : pad_sequences(x, maxlen=max_len, truncating=trun, padding=padd),
+            seq_list
+        ))
+
+    def convert_to_one_hot(self, padded):
+        one_hot_vector = np.zeros((len(padded), self.padding_size, self.get_vocab_size()))
+
+
+        for i, sequence in enumerate(padded):
+            for j, index in enumerate(sequence):
+                one_hot_vector[i, j, index] = 1
+
+        return one_hot_vector
